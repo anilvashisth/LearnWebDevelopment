7f0980843a8f31141dba3628ab9a8a3dc76dcb31
anilvashisth <80627006+anilvashisth@users.noreply.github.com>
2025-06-27 21:35:10 +0530

    Create class6.js

---
+++ javascript/class6.js
@@ -0,0 +1,10 @@
+How to change the mobile no in Stamp paper after purchage
+Case 1 : 4 marla jameen means (30.543220585) 30.543X4=122.173 guj and
+122.17x3600= 439812
+7% of Amt = 30786.84+1000(Fee)
+
+or 
+Case 2 : 4 marla jameen means (30.25) 30.25x4=121 guj 
+121x3600=435600 (436000)
+7% of amt 435600 = 30492
+7% of amt 436000 = 30520 Approx 30600+1000=31600+500+32100
