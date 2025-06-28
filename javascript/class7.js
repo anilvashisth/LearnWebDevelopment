e5278ca59fba8cadb56c10753101cabb3dc8d18e
anilvashisth <80627006+anilvashisth@users.noreply.github.com>
2025-06-28 22:03:42 +0530

    Create class7.js

---
+++ javascript/class7.js
@@ -0,0 +1,13 @@
+1 killa = 8 Kanal
+1 kanal = 20 marla
+1 Killa = 8x20 = 160 marla
+North and South 40 Karam
+East and West 36 Karam
+40x36=1440/9=160 marla
+Killa No 23/10
+North 2+3+35=40
+South 2+30+5+3=40
+East 36
+west 3+33=36
+north (dhana=2Karam) 2x36=72/9=8 marla
+north Ghor(3karamx3karam) 3x3=9/9=1 karam
