422d0b25c8ed7451c8a9235e5efc5941b96f2555
anilvashisth <80627006+anilvashisth@users.noreply.github.com>
2025-06-29 21:16:28 +0530

    Create class8.js

---
+++ javascript/class8.js
@@ -0,0 +1,5 @@
+From last few months 
+want to give this tree to all of my villager.
+on dated 28th june 2025 ram prasad took two tree
+and on dated 29th june 2025 one tree has given to beru panna man.
+  
