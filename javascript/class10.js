2d5fcc6b45384fb2db228b879ce1929578751722
anilvashisth <80627006+anilvashisth@users.noreply.github.com>
2025-07-01 21:32:03 +0530

    Create class10.js

---
+++ javascript/class10.js
@@ -0,0 +1,7 @@
+How to Stop Black Money (Unaccounted Money) in Agriculture Land Purchase
+Have to Prove agriculture land
+with bill of purchase of Beej 
+bank tranction 
+Payment of majdoor
+receipt of Mandy
+Electricity Bill
