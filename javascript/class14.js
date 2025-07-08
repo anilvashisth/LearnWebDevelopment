f3b64b1e02af6a89a276ff52b9040d6be0ee5e88
anilvashisth <80627006+anilvashisth@users.noreply.github.com>
2025-07-08 21:53:42 +0530

    Create class14.js

---
+++ javascript/class14.js
@@ -0,0 +1,7 @@
+go for dohli
+20 year jamabandi
+what can we do if any nakal is not online  (Khewat no 36 2012-13) 
+20 year girdawari
+Next Problem is Jage ram Plot 18 marla
+badar is required
+and one of the plot not online
