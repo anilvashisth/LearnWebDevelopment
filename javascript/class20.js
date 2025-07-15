af1b86fa3c04bb2ee71974c2f5d610928f85e617
anilvashisth <80627006+anilvashisth@users.noreply.github.com>
2025-07-15 21:08:12 +0530

    Create class20.js

---
+++ javascript/class20.js
@@ -0,0 +1,7 @@
+1 Take a Token for Nakal
+instead of Padat Patwar nakal online patwar is required and taken.
+114 no Khewat.
+Shyam has given a intkaal receipt of JP.
+collected Registry of 4 marla from Sonu.
+Given a print out to Patwari.
+Photo copy of agriculture Copy and bandobust of Agri Land and plot.  
